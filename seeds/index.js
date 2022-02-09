158d897802d1e6603803949d102ed5a0e21af8bf
Your Name <you@example.com>
2022-02-09 12:57:08 -0500

    added index.js in models

--- seeds/index.js
+++ seeds/index.js
@@ -5,13 +5,14 @@ const sequelize = require('../config/connection');
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
-  console.log('--------------');
+  console.log('-------Database synced-------');
+  
   await seedUsers();
-  console.log('--------------');
+  console.log('-------Users seeded--------');
 
   await seedPosts();
-  console.log('--------------');
-  
+  console.log('--------Posts seeded-------');
+
   process.exit(0);
 };
 

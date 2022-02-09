c8ffccd6404e6e40226c4c507734518c40ec4077
Your Name <you@example.com>
2022-02-09 16:39:10 -0500

    added handlebars files

--- models/index.js
+++ models/index.js
@@ -1,6 +1,9 @@
-const User = require('./User');
+// import all models
 const Post = require('./Post');
+const User = require('./User');
+const Comment = require ('./Comment');
 
+// associations
 User.hasMany(Post, {
   foreignKey: 'user_id'
 });
@@ -9,4 +12,20 @@ Post.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-module.exports = { User, Post };
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Comment };
